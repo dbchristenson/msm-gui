7e13179c47afc06e474170ec6f074be7cca561df
DB Christenson <dbeauchristenson@gmail.com>
2024-05-17 21:00:54 +0000

    the movies are ready

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,4 +14,47 @@ class MoviesController < ApplicationController
 
     render({ :template => "movie_templates/show" })
   end
+
+  def insert
+    movie = Movie.new
+    movie.title = params.fetch("query_title")
+    movie.year = params.fetch("query_year")
+    movie.description = params.fetch("query_description")
+    movie.image = params.fetch("query_image")
+    movie.director_id = params.fetch("query_director_id")
+
+    if movie.save
+      redirect_to("/movies", { :notice => "Movie added successfully." })
+    else
+      redirect_to("/insert_movie", { :alert => "Failed to add movie." })
+    end
+  end
+
+  def modify
+    the_id = params.fetch("path_id")
+    movie = Movie.where({ :id => the_id }).first
+    movie.title = params.fetch("query_title")
+    movie.year = params.fetch("query_year")
+    movie.description = params.fetch("query_description")
+    movie.image = params.fetch("query_image")
+    movie.director_id = params.fetch("query_director_id")
+
+    if movie.save
+      redirect_to("/movies/#{movie.id}", { :notice => "Movie updated successfully." })
+    else
+      redirect_to("/modify_movie/#{movie.id}", { :alert => "Failed to update movie." })
+    end
+  end
+
+  def delete
+    the_id = params.fetch("path_id")
+    movie = Movie.where({ :id => the_id }).first
+
+    if movie.destroy
+      redirect_to("/movies", { :notice => "Movie deleted successfully." })
+    else
+      redirect_to("/movies", { :alert => "Failed to delete movie." })
+    end
+  end
+
 end
